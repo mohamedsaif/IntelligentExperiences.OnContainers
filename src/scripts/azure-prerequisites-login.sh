2102dccd74fb3a6a474b62098495bf315f0036f0
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-12 22:26:50 +0400

    adding first guides

---
+++ src/scripts/azure-prerequisites-login.sh
@@ -0,0 +1,28 @@
+#***** Login to Azure Subscription *****
+# A browser window will open to complete the authentication :)
+az login
+
+# You can also login using Service Principal (replace values in <>)
+# az login --service-principal --username APP_ID --password PASSWORD --tenant TENANT_ID
+
+az account set --subscription "YOUR-SUBSCRIPTION-NAME"
+
+#Make sure the active subscription is set correctly
+SUBSCRIPTION_ACCOUNT=$(az account show)
+echo $SUBSCRIPTION_ACCOUNT
+
+# Get the tenant ID
+TENANT_ID=$(echo $SUBSCRIPTION_ACCOUNT | jq -r .tenantId)
+# or use TENANT_ID=$(az account show --query tenantId -o tsv)
+echo $TENANT_ID
+echo export TENANT_ID=$TENANT_ID >> ~/.bashrc
+
+# Get the subscription ID
+SUBSCRIPTION_ID=$(echo $SUBSCRIPTION_ACCOUNT | jq -r .id)
+# or use TENANT_ID=$(az account show --query tenantId -o tsv)
+echo $SUBSCRIPTION_ID
+echo export SUBSCRIPTION_ID=$SUBSCRIPTION_ID >> ~/.bashrc
+
+clear
+
+#***** END Login to Azure Subscription *****

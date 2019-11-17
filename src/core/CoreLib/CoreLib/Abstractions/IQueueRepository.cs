10e625b07b704ce317dde1f232c678d67800474b
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-17 09:41:16 +0400

    Adding abstractions to CoreLib

---
+++ src/core/CoreLib/CoreLib/Abstractions/IQueueRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.Abstractions
+{
+    public interface IQueueRepository<T>
+    {
+        Task<bool> QueueMessage(T message);
+        Task<T> GetMessage();
+        Task<bool> DeleteMessage(T message);
+        Task<int> GetQueueLength();
+    }
+}
---
+++ src/core/CoreLib/CoreLib/Abstractions/IStorageRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.Abstractions
+{
+    public interface IStorageRepository
+    {
+        Task<string> CreateFileAsync(string name, byte[] fileData);
+        Task<string> CreateFileAsync(string name, Stream fileData);
+        Task<string> CreateFileAsync(string containerName, string fileName, Stream fileData);
+        Task<byte[]> GetFileAsync(string fileName);
+        Task<byte[]> GetFileAsync(string containerName, string fileName);
+        string GetFileDownloadUrl(string fileName);
+    }
+}

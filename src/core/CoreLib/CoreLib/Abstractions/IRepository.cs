834cdbcd81827264f3b8f21cef62bfad9e2783fc
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-17 09:41:16 +0400

    Adding abstractions to CoreLib

---
+++ src/core/CoreLib/CoreLib/Abstractions/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using CoreLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.Abstractions
+{
+    /// <summary>
+    /// Following the repository design pattern which isolates data access behind interface abstractions
+    /// </summary>
+    /// <typeparam name="T">Entity that hold the data</typeparam>
+    public interface IRepository<T> where T : BaseModel
+    {
+        Task<T> GetByIdAsync(string id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}

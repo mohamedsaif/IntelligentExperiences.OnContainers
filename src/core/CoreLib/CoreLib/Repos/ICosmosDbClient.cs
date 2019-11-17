3d602ab8690f332e7bc8063cf12a1f8c308624c1
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-17 15:04:46 +0400

    Refactoring CoreLib Repos

---
+++ src/core/CoreLib/CoreLib/Repos/ICosmosDbClient.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreLib.Repos
+{
+    public interface ICosmosDbClient
+    {
+        Task<Document> ReadDocumentAsync(string documentId, RequestOptions options = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<Document> CreateDocumentAsync(object document, RequestOptions options = null,
+            bool disableAutomaticIdGeneration = false,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<Document> ReplaceDocumentAsync(string documentId, object document, RequestOptions options = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<Document> DeleteDocumentAsync(string documentId, RequestOptions options = null,
+            CancellationToken cancellationToken = default(CancellationToken));
+    }
+}

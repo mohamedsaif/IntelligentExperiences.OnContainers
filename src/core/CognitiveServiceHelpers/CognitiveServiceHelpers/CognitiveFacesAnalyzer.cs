06b8d1f809b1f03054165106b70b08ef02441d18
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-20 10:25:27 +0400

    Adding CognitiveServicesHelper

---
+++ src/core/CognitiveServiceHelpers/CognitiveServiceHelpers/CognitiveFacesAnalyzer.cs
@@ -0,0 +1,155 @@
+﻿using CognitiveServiceHelpers.Models;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServiceHelpers
+{
+    public class CognitiveFacesAnalyzer
+    {
+        public bool ShowDialogOnFaceApiErrors { get; set; } = false;
+        public bool FilterOutSmallFaces { get; set; } = false;
+
+        private static readonly FaceAttributeType[] DefaultFaceAttributeTypes = new FaceAttributeType[]
+        {
+            FaceAttributeType.Age,
+            FaceAttributeType.Gender,
+            FaceAttributeType.Emotion
+        };
+
+        public event EventHandler FaceDetectionCompleted;
+        public event EventHandler FaceRecognitionCompleted;
+
+        public static string PeopleGroupsUserDataFilter = null;
+        public Func<Task<Stream>> GetImageStreamCallback { get; set; }
+
+        public IEnumerable<DetectedFace> DetectedFaces { get; set; }
+
+        public IEnumerable<IdentifiedPerson> IdentifiedPersons { get; set; }
+
+        public IEnumerable<SimilarFaceMatch> SimilarFaceMatches { get; set; }
+
+        public async Task IdentifyFacesAsync()
+        {
+            this.IdentifiedPersons = Enumerable.Empty<IdentifiedPerson>();
+
+            Guid[] detectedFaceIds = this.DetectedFaces?.Where(f => f.FaceId.HasValue).Select(f => f.FaceId.GetValueOrDefault()).ToArray();
+            if (detectedFaceIds != null && detectedFaceIds.Any())
+            {
+                List<IdentifiedPerson> result = new List<IdentifiedPerson>();
+
+                IEnumerable<PersonGroup> personGroups = Enumerable.Empty<PersonGroup>();
+                try
+                {
+                    personGroups = await FaceServiceHelper.ListPersonGroupsAsync(PeopleGroupsUserDataFilter);
+                }
+                catch (Exception e)
+                {
+                    ErrorTrackingHelper.TrackException(e, "Face API GetPersonGroupsAsync error");
+
+                    if (this.ShowDialogOnFaceApiErrors)
+                    {
+                        await ErrorTrackingHelper.GenericApiCallExceptionHandler(e, "Failure getting PersonGroups");
+                    }
+                }
+
+                foreach (var group in personGroups)
+                {
+                    try
+                    {
+                        IList<IdentifyResult> groupResults = await FaceServiceHelper.IdentifyAsync(group.PersonGroupId, detectedFaceIds);
+                        foreach (var match in groupResults)
+                        {
+                            if (!match.Candidates.Any())
+                            {
+                                continue;
+                            }
+
+                            Person person = await FaceServiceHelper.GetPersonAsync(group.PersonGroupId, match.Candidates[0].PersonId);
+
+                            IdentifiedPerson alreadyIdentifiedPerson = result.FirstOrDefault(p => p.Person.PersonId == match.Candidates[0].PersonId);
+                            if (alreadyIdentifiedPerson != null)
+                            {
+                                // We already tagged this person in another group. Replace the existing one if this new one if the confidence is higher.
+                                if (alreadyIdentifiedPerson.Confidence < match.Candidates[0].Confidence)
+                                {
+                                    alreadyIdentifiedPerson.Person = person;
+                                    alreadyIdentifiedPerson.Confidence = match.Candidates[0].Confidence;
+                                    alreadyIdentifiedPerson.FaceId = match.FaceId;
+                                }
+                            }
+                            else
+                            {
+                                result.Add(new IdentifiedPerson { Person = person, Confidence = match.Candidates[0].Confidence, FaceId = match.FaceId });
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // Catch errors with individual groups so we can continue looping through all groups. Maybe an answer will come from
+                        // another one.
+                        ErrorTrackingHelper.TrackException(e, "Face API IdentifyAsync error");
+
+                        if (this.ShowDialogOnFaceApiErrors)
+                        {
+                            await ErrorTrackingHelper.GenericApiCallExceptionHandler(e, "Failure identifying faces");
+                        }
+                    }
+                }
+
+                this.IdentifiedPersons = result;
+            }
+
+            this.OnFaceRecognitionCompleted();
+        }
+
+        public async Task FindSimilarPersistedFacesAsync()
+        {
+            this.SimilarFaceMatches = Enumerable.Empty<SimilarFaceMatch>();
+
+            if (this.DetectedFaces == null || !this.DetectedFaces.Any())
+            {
+                return;
+            }
+
+            List<SimilarFaceMatch> result = new List<SimilarFaceMatch>();
+
+            foreach (DetectedFace detectedFace in this.DetectedFaces)
+            {
+                try
+                {
+                    SimilarFace similarPersistedFace = await FaceListManager.FindSimilarPersistedFaceAsync(this.GetImageStreamCallback, detectedFace.FaceId.GetValueOrDefault(), detectedFace);
+                    if (similarPersistedFace != null)
+                    {
+                        result.Add(new SimilarFaceMatch { Face = detectedFace, SimilarPersistedFace = similarPersistedFace });
+                    }
+                }
+                catch (Exception e)
+                {
+                    ErrorTrackingHelper.TrackException(e, "FaceListManager.FindSimilarPersistedFaceAsync error");
+
+                    if (this.ShowDialogOnFaceApiErrors)
+                    {
+                        await ErrorTrackingHelper.GenericApiCallExceptionHandler(e, "Failure finding similar faces");
+                    }
+                }
+            }
+
+            this.SimilarFaceMatches = result;
+        }
+
+        private void OnFaceDetectionCompleted()
+        {
+            this.FaceDetectionCompleted?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnFaceRecognitionCompleted()
+        {
+            this.FaceRecognitionCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ src/core/CognitiveServiceHelpers/CognitiveServiceHelpers/Models/IdentifiedPerson.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServiceHelpers.Models
+{
+    public class IdentifiedPerson
+    {
+        public double Confidence
+        {
+            get; set;
+        }
+
+        public Person Person
+        {
+            get; set;
+        }
+
+        public Guid FaceId
+        {
+            get; set;
+        }
+    }
+}

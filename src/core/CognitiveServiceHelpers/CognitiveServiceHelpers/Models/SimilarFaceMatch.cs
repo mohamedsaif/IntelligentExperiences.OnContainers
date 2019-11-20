d73c06be51defe69e2ce004ce1d6c2b98b1c4e93
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-20 10:25:27 +0400

    Adding CognitiveServicesHelper

---
+++ src/core/CognitiveServiceHelpers/CognitiveServiceHelpers/Models/SimilarFaceMatch.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServiceHelpers.Models
+{
+    public class SimilarFaceMatch
+    {
+        public DetectedFace Face
+        {
+            get; set;
+        }
+
+        public SimilarFace SimilarPersistedFace
+        {
+            get; set;
+        }
+    }
+}

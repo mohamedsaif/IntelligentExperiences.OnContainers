0b495bdb20081ca5f40737a7608a92cb1e55cfb7
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-21 09:51:53 +0400

    Refactoring

--- src/services/CrowdAnalyzer/CrowdAnalyzer.cs
+++ src/services/CrowdAnalyzer/CrowdAnalyzer.cs
@@ -1,21 +1,50 @@
 using System;
+using System.Threading.Tasks;
+using CrowdAnalyzer.Abstractions;
+using CrowdAnalyzer.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CrowdAnalyzer.Functions
 {
-    public static class CrowdAnalyzer
+    public class CrowdAnalyzer
     {
+        private IVisitorsRepository visitorRepo;
+        private ICrowdDemographicsRepository crowdDemographicsRepo;
+
+        public CrowdAnalyzer(
+            IVisitorsRepository vRepo,
+            ICrowdDemographicsRepository cRepo)
+        {
+            visitorRepo = vRepo;
+            crowdDemographicsRepo = cRepo;
+        }
+
         [FunctionName("CrowdAnalyzer")]
-        public static void Run(
+        [return: ServiceBus(queueOrTopicName: AppConstants.SBTopicDemographics,
+            entityType: Microsoft.Azure.WebJobs.ServiceBus.EntityType.Topic,
+            Connection = "serviceBusConnection")]
+        public async Task<string> Run(
             [ServiceBusTrigger("crowd-analysis", "crowd-analyzer", Connection = "serviceBusConnection")]string request, 
             ILogger log)
         {
             DateTime startTime = DateTime.UtcNow;
             log.LogInformation($"FUNC (CrowdAnalyzer): crowd-analysis topic triggered processing message: {JsonConvert.SerializeObject(request)}");
 
-            
+            CamFrameAnalysis analysis = null;
+
+            try
+            {
+                analysis = JsonConvert.DeserializeObject<CamFrameAnalysis>(request);
+            }
+            catch(Exception e)
+            {
+
+            }
+
+            return null;
         }
     }
 }

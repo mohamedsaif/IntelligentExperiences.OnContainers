5acafd91ba494662303b2c7ccd7ded0093fe821b
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2019-11-28 12:11:28 +0400

    Updated Cam Defice Project

--- src/iot/Cam.Device.Web/Controllers/HomeController.cs
+++ src/iot/Cam.Device.Web/Controllers/HomeController.cs
@@ -6,20 +6,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Cam.Device.Web.Models;
+using Microsoft.Extensions.Options;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Cam.Device.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<AppSettings> _appSettings;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private string deviceId;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSettings> settings, IWebHostEnvironment env)
         {
             _logger = logger;
+            _appSettings = settings;
+            _hostingEnvironment = env;
+            deviceId = _appSettings.Value.DeviceId;
         }
 
         public IActionResult Index()
         {
+            ViewData["DeviceId"] = deviceId;
             return View();
         }
 
@@ -33,5 +44,66 @@ namespace Cam.Device.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult CaptureFrame()
+        {
+            try
+            {
+
+                var files = HttpContext.Request.Form.Files;
+                if (files != null)
+                {
+                    foreach (var file in files)
+                    {
+                        if (file.Length > 0)
+                        {
+                            // Getting Filename
+                            var fileName = file.FileName;
+                            
+                            // Getting Extension
+                            var fileExtension = Path.GetExtension(fileName);
+
+                            // Always save the file with the same name to simulate a stream
+                            // Use deviceId + fileExtension as the file name
+                            var newFileName = string.Concat(deviceId, fileExtension);
+                            //  Generating physical path to store photo 
+                            var filepath = Path.Combine(_hostingEnvironment.WebRootPath, "CamFrames") + $@"\{newFileName}";
+
+                            if (!string.IsNullOrEmpty(filepath))
+                            {
+                                // Save the frame
+                                SaveFrame(file, filepath);
+                            }
+                        }
+                    }
+                    return Json(true);
+                }
+                else
+                {
+                    return Json(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Saving captured image into Folder.
+        /// </summary>
+        /// <param name="file">Actual image file sent in the request</param>
+        /// <param name="fileName">File name</param>
+        private void SaveFrame(IFormFile file, string fileName)
+        {
+            using (FileStream fs = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+        }
+
     }
 }
---
+++ src/iot/Cam.Device.Web/wwwroot/js/userMedia.js
@@ -0,0 +1,187 @@
+﻿(function () {
+    'use strict';
+
+    var mediaStream = null;
+    var webcamList = [];
+    var currentCam = null;
+    var photoReady = false;
+    var uploadedCount = 1;
+
+    var init = function () {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        document.getElementById('switch').addEventListener('click', nextWebCam, false);
+
+        if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+            navigator.mediaDevices.enumerateDevices().then(devicesCallback);
+        }
+        else if (navigator.getUserMedia) {
+            document.getElementById('tooltip').innerHTML = 'Cannot switch web cams because navigator.mediaDevices.enumerateDevices is unsupported by your browser.';
+
+            navigator.getUserMedia({ video: true /*, audio: true */ }, initializeVideoStream, getUserMediaError);
+        }
+        else {
+            writeError('You are using a browser that does not support the Media Capture API');
+        }
+    };
+
+    var initializeVideoStream = function (stream) {
+        mediaStream = stream;
+
+        var video = document.getElementById('videoTag');
+        if (typeof (video.srcObject) !== 'undefined') {
+            video.srcObject = mediaStream;
+        }
+        else {
+            video.src = URL.createObjectURL(mediaStream);
+        }
+        if (webcamList.length > 1) {
+            document.getElementById('switch').disabled = false;
+            ul.children[currentCam].setAttribute("class", "list-group-item active");
+        }
+        capture();
+    };
+
+    var savePhoto = function () {
+        if (photoReady) {
+            var canvas = document.getElementById('canvasTag');
+            canvas.toBlob(function (blob) {
+                if (blob == null)
+                    return;
+
+                //Upload to server
+                var data = new FormData();
+                data.append('frame', blob, 'deviceId' + uploadedCount + "." + 'jpg');
+                $.ajax({
+                    type: "POST",
+                    url: "/Home/CaptureFrame",
+                    data: data,
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                    success: function (data) {
+                        document.getElementById('uploadedViewText').innerHTML = 'Uploaded Count: ' + uploadedCount;
+                    },
+                    error: function (data) {
+                        document.getElementById('uploadedViewText').innerHTML = data;
+                    }
+                });
+
+                uploadedCount += 1;
+                setTimeout(capture, 3000);
+            });
+        }
+    };
+
+    var capture = function () {
+
+        if (!mediaStream) {
+            photoReady = false;
+            return;
+        }
+
+        var video = document.getElementById('videoTag');
+        var canvas = document.getElementById('canvasTag');
+        
+        var videoWidth = video.videoWidth;
+        var videoHeight = video.videoHeight;
+
+        if (canvas.width !== videoWidth || canvas.height !== videoHeight) {
+            canvas.width = videoWidth;
+            canvas.height = videoHeight;
+        }
+
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        photoReady = true;
+        document.getElementById('photoViewText').innerHTML = 'Last captured frame: ' + Date();
+        savePhoto();
+    };
+
+    var nextWebCam = function () {
+        document.getElementById('switch').disabled = true;
+        if (currentCam !== null) {
+            currentCam++;
+            if (currentCam >= webcamList.length) {
+                currentCam = 0;
+            }
+            var video = document.getElementById('videoTag');
+            video.srcObject = null;
+            video.src = null;
+            if (mediaStream) {
+                var videoTracks = mediaStream.getVideoTracks();
+                videoTracks[0].stop();
+                mediaStream = null;
+            }
+        }
+        else {
+            currentCam = 0;
+        }
+
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                width: 1280,
+                height: 720,
+                deviceId: { exact: webcamList[currentCam] }
+            }
+        }).then(initializeVideoStream)
+            .catch(getUserMediaError);
+    };
+
+    var deviceChanged = function () {
+        navigator.mediaDevices.removeEventListener('devicechange', deviceChanged);
+        // Reset the webcam list and re-enumerate
+        webcamList = [];
+        /*eslint-disable*/
+        navigator.mediaDevices.enumerateDevices().then(devicesCallback);
+        /*eslint-enable*/
+    };
+
+    var devicesCallback = function (devices) {
+        var ul = document.getElementById("cameras");
+        ul.innerHTML = "";
+        // Identify all webcams
+        for (var i = 0; i < devices.length; i++) {
+            if (devices[i].kind === 'videoinput') {
+                webcamList[webcamList.length] = devices[i].deviceId;
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode(devices[i].deviceId));
+                ul.appendChild(li);
+            }
+        }
+
+        if (webcamList.length > 0) {
+            // Start video with the first device on the list
+            nextWebCam();
+            if (webcamList.length > 1) {
+                document.getElementById('switch').disabled = false;
+            }
+            else {
+                document.getElementById('switch').disabled = true;
+            }
+        }
+        else {
+            writeError('Webcam not found.');
+        }
+        navigator.mediaDevices.addEventListener('devicechange', deviceChanged);
+    };
+
+    var writeError = function (string) {
+        var elem = document.getElementById('error');
+        var p = document.createElement('div');
+        p.appendChild(document.createTextNode('ERROR: ' + string));
+        elem.appendChild(p);
+    };
+
+    var getUserMediaError = function (e) {
+        if (e.name.indexOf('NotFoundError') >= 0) {
+            writeError('Webcam not found.');
+        }
+        else {
+            writeError('The following error occurred: "' + e.name + '" Please check your webcam device(s) and try again.');
+        }
+    };
+
+    init();
+
+}());

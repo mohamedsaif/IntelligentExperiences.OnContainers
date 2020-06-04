871e2f10a7a6c38d91b99ffc711a31da26d7b3f9
Mohamed Saif (B2) <mohamed.saif@outlook.com>
2020-06-04 10:51:16 +0400

    Updated identification APIs

--- src/apis/CognitiveOrchestrator.API/Controllers/IdentificationController.cs
+++ src/apis/CognitiveOrchestrator.API/Controllers/IdentificationController.cs
@@ -45,39 +45,110 @@ namespace CognitiveOrchestrator.API.Controllers
         }
 
         // Visitors groups
-        [HttpGet("groups/get/{groupId}")]
-        [ProducesResponseType(typeof(BaseResponse), 200)]
-        public IActionResult GetVisitrosGroupById(string groupId)
+        [HttpGet("groups/getById/{groupId}")]
+        [ProducesResponseType(typeof(IdentifiedVisitorGroup), 200)]
+        public async Task<IActionResult> GetVisitrosGroupById(string groupId)
         {
-            
-            return Ok(new BaseResponse
+            try
             {
-                IsSuccessful = true,
-                Message = "Service is running...",
-                StatusCode = "0"
-            });
+                var result = await visitorIdentificationManager.GetVisitorsGroupByIdAsync(groupId);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
         }
+
+        [HttpGet("groups/getByName/{groupName}")]
+        [ProducesResponseType(typeof(IdentifiedVisitorGroup), 200)]
+        public async Task<IActionResult> GetVisitrosGroupByName(string groupName)
+        {
+            try
+            {
+                var result = await visitorIdentificationManager.GetVisitorsGroupByNameAsync(groupName);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
+        }
+
+        [HttpGet("groups/getAll")]
+        [ProducesResponseType(typeof(List<IdentifiedVisitorGroup>), 200)]
+        public async Task<IActionResult> GetAllVisitorsGroups()
+        {
+            try
+            {
+                var result = await visitorIdentificationManager.GetAllVisitorsGroupsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
+        }
+
         [HttpPost("groups/create/{groupName}")]
         [ProducesResponseType(typeof(IdentifiedVisitorGroup), 200)]
         public async Task<IActionResult> CreateVisitorsGroup(string groupName)
         {
-            // Validation of input
-            var result = await visitorIdentificationManager.CreateVisitorsGroupAsync(groupName);
-            return Ok(result);
+            try
+            {
+                var result = await visitorIdentificationManager.CreateVisitorsGroupAsync(groupName);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
         }
 
         [HttpPost("groups/train/{groupId}")]
         [ProducesResponseType(typeof(BaseResponse), 200)]
         public async Task<IActionResult> TrainVisitorsGroup(string groupId)
         {
-            // Validation of input
-            await visitorIdentificationManager.TrainVisitorGroup(groupId, true);
-            return Ok(new BaseResponse
+            try
             {
-                IsSuccessful = true,
-                Message = "Training successfully completed.",
-                StatusCode = "0"
-            });
+                await visitorIdentificationManager.TrainVisitorGroup(groupId, true);
+                return Ok(new BaseResponse
+                {
+                    IsSuccessful = true,
+                    Message = "Training successfully completed.",
+                    StatusCode = "0"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
+            
         }
 
         [HttpPost("groups/delete/{groupId}")]
@@ -106,10 +177,10 @@ namespace CognitiveOrchestrator.API.Controllers
         }
 
         // Visitors
-        [HttpPost("create-visitor")]
+        [HttpPost("visitors/create")]
         [Consumes("multipart/form-data")]
         [ProducesResponseType(typeof(IdentifiedVisitor), 200)]
-        public async Task<IActionResult> CreateVisitorAsync(IFormCollection data)
+        public async Task<IActionResult> CreateVisitor(IFormCollection data)
         {
             //var visitorSample = new IdentifiedVisitor
             //{
@@ -152,6 +223,71 @@ namespace CognitiveOrchestrator.API.Controllers
             return Ok(JsonConvert.SerializeObject(newVisitor));
         }
 
+        [HttpGet("visitors/getById/{id}")]
+        [ProducesResponseType(typeof(IdentifiedVisitor), 200)]
+        public async Task<IActionResult> GetVisitorById(string id)
+        {
+            try
+            {
+                var result = await visitorIdentificationManager.GetVisitorByIdAsync(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
+        }
 
+        [HttpGet("visitors/getAll")]
+        [ProducesResponseType(typeof(List<IdentifiedVisitor>), 200)]
+        public async Task<IActionResult> GetAllVisitors()
+        {
+            try
+            {
+                var result = await visitorIdentificationManager.GetAllIdentifiedVisitorsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse
+                {
+                    IsSuccessful = false,
+                    Message = $"Error: {ex.Message}",
+                    StatusCode = "1"
+                });
+            }
+        }
+
+        [HttpPost("visitors/update")]
+        [Consumes("multipart/form-data")]
+        [ProducesResponseType(typeof(IdentifiedVisitor), 200)]
+        public async Task<IActionResult> UpdateVisitor(IFormCollection data)
+        {
+            var visitorJson = data["visitor"];
+            var visitor = JsonConvert.DeserializeObject<IdentifiedVisitor>(visitorJson);
+            visitor.Photos = new List<VisitorPhoto>();
+            foreach (var photo in data.Files)
+            {
+                VisitorPhoto newPhoto = new VisitorPhoto
+                {
+                    Name = photo.FileName,
+                    IsSaved = false,
+                    Status = "Submitted"
+                };
+                var photoDataStream = new MemoryStream();
+                photo.CopyTo(photoDataStream);
+                newPhoto.PhotoData = photoDataStream.ToArray();
+                visitor.Photos.Add(newPhoto);
+            }
+
+            visitor = await visitorIdentificationManager.UpdateVisitorAsync(visitor);
+
+            return Ok(visitor);
+        }
     }
 }

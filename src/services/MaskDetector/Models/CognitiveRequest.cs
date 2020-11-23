fcd260a25c02111000da6f7120c17286ee5c4b50
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2020-11-23 04:46:22 +0400

    Added initial models

---
+++ src/services/MaskDetector/Models/CognitiveRequest.cs
@@ -0,0 +1,21 @@
+﻿using CoreLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskDetector.Models
+{
+    public class CognitiveRequest : BaseModel
+    {
+        public string FileUrl { get; set; }
+        public string DeviceId { get; set; }
+        public DateTime TakenAt { get; set; }
+        public DateTime ProcessedAt { get; set; }
+        public string TargetAction { get; set; }
+        public string Status { get; set; }
+        public bool IsProcessed { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ src/services/MaskDetector/Models/MaskAnalysis.cs
@@ -0,0 +1,27 @@
+﻿using CoreLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskDetector.Models
+{
+    public class MaskAnalysis : BaseModel
+    {
+        public string TimeKey { get; set; }
+        public CognitiveRequest Request { get; set; }
+
+        public MaskDetectionResult DetectionResult { get; set; }
+
+        public int TotalDetected { get; set; }
+        public int TotalDetectedWithMasks { get; set; }
+        public int TotalDetectedWithoutMasks { get; set; }
+
+        // Processing status
+        public bool IsSuccessful { get; set; }
+        public string Status { get; set; }
+        public int TotalProcessingTime { get; set; }
+
+    }
+}

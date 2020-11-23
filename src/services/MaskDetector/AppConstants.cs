37643d5bdb57798aea457b657e81129fd1ee3265
Mohamed Saif (Aero) <mohamed.saif@outlook.com>
2020-11-23 05:37:23 +0400

    Added db setup for mask detector

---
+++ src/services/MaskDetector/AppConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskDetector
+{
+    /// <summary>
+    /// Hold a central list of all static configurations that can remain the same across all deployments
+    /// </summary>
+    public static class AppConstants
+    {
+        //Remember always that blob storage containers allows only lower case letters
+        public const string StorageContainerName = "camframefiles";
+
+        //Cosmos Db settings
+        public const string DbName = "cognitive-analysis-db";
+        public const string DbColMaskAnalysis = "mask-analysis";
+        public const string DbColMaskAnalysisPartitionKey = "TimeKey";
+
+        //Main Service Bus settings
+        public const string SBTopic = "camframe-analysis";
+        public const string SBSubscription = "mask-detector";
+    }
+}
---
+++ src/services/MaskDetector/Repos/MaskAnalysisRepository.cs
@@ -0,0 +1,24 @@
+﻿using CoreLib.Repos;
+using MaskDetector.Abstractions;
+using MaskDetector.Models;
+using Microsoft.Azure.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskDetector.Repos
+{
+    public class MaskAnalysisRepository : CosmosDbRepository<MaskAnalysis>, IMaskAnalysisRepository
+    {
+        public MaskAnalysisRepository(ICosmosDbClientFactory cosmosDbClientFactory) : base(cosmosDbClientFactory) { }
+
+        public override string CollectionName { get; } = AppConstants.DbColMaskAnalysis;
+
+        public override string GenerateId(MaskAnalysis entity) => $"{Guid.NewGuid()}";
+
+        // Initially I opted to use month-year as the partition key. You can partition the data in different way.
+        public override PartitionKey ResolvePartitionKey(string entityId) => new PartitionKey($"{entityId.Substring(entityId.LastIndexOf('-') + 1)}");
+    }
+}
